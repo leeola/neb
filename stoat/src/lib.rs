f21ebf9a74dc14edd6c8ebe4bf8f47f416a4ec6b
Lee Olayvar <leegit@fastmail.com>
2024-08-31 10:01:56 -0700

    wip, stash

--- stoat/src/lib.rs
+++ stoat/src/lib.rs
@@ -1,12 +1,26 @@
+use action::Action;
+
 pub mod buffer {
     pub struct Buffer;
 }
 pub mod session {}
+pub mod action {
+    pub enum Action {}
+}
+pub mod message {
+    pub enum Message {}
+}
 
 #[derive(Default)]
 pub struct Stoat;
 impl Stoat {
     pub fn new() -> Self {
         Self
+    }
+    pub fn input(&self) {
+        todo!()
+    }
+    pub fn input_echo(&self) -> Action {
+        todo!()
     }
 }
